a9a0a169052c95e0a3f31a8c2200772c035f149e
josef103 <mario.popescu@gmail.com>
2014-08-06 21:06:47 +0300

    added a test class

---
+++ testFile.h
@@ -0,0 +1,13 @@
+//
+//  testFile.h
+//  Git Test
+//
+//  Created by Mario Popescu on 8/6/14.
+//  Copyright (c) 2014 Mario Popescu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface testFile : NSObject
+
+@end
---
+++ testFile.m
@@ -0,0 +1,13 @@
+//
+//  testFile.m
+//  Git Test
+//
+//  Created by Mario Popescu on 8/6/14.
+//  Copyright (c) 2014 Mario Popescu. All rights reserved.
+//
+
+#import "testFile.h"
+
+@implementation testFile
+
+@end
